d15b0c16ac031698562b260fc81b24250f17ac3a
Dominic Reinmann <reinmann.dominic@bluewin.ch>
2022-08-07 13:18:41 +0200

    ver 0.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React from "react";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Feauters,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { Brand, CTA, Feauter, Navbar } from "./components";
 
 const App = () => {
   return (
